9fd296809f0421893cd1081ad832bc5651d0ab1a
Ugo Nicolai <unicolai@student.42.fr>
2017-12-26 17:13:30 +0100

    Smoke green everyday

--- srcs/ft_read_content.c
+++ srcs/ft_read_content.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 15:16:18 by jjaniec           #+#    #+#             */
-/*   Updated: 2017/12/26 16:57:54 by unicolai         ###   ########.fr       */
+/*   Updated: 2017/12/26 17:11:34 by unicolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_resize_buf(int fd, char *buf, int *bufsize_multiplier, int *wrote)
 	char	*buf2;
 
 	if (!(buf2 = (char *)malloc(sizeof(char) *
-	BUFF_SIZE * *bufsize_multiplier + 1)))
+	21 * *bufsize_multiplier + 1)))
 		return (NULL);
-	*wrote = read(fd, buf2, (BUFF_SIZE * *bufsize_multiplier));
+	*wrote = read(fd, buf2, (21 * *bufsize_multiplier));
 	buf2[*wrote] = '\0';
 	*bufsize_multiplier += 1;
 	return (ft_strjoin(buf, buf2));
@@ -32,11 +32,11 @@ char	*ft_read_content(int fd)
 	int		multiplier;
 
 	multiplier = 1;
-	if (!(buf = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
+	if (!(buf = (char *)malloc(sizeof(char) * 21 + 1)))
 		return (NULL);
-	x = read(fd, buf, BUFF_SIZE);
-	buf[x] ='\0';
-	if (x < BUFF_SIZE)
+	x = read(fd, buf, 21);
+	buf[x] = '\0';
+	if (x < 21)
 		return (buf);
 	while (x > 0)
 		buf = ft_resize_buf(fd, buf, &multiplier, &x);
